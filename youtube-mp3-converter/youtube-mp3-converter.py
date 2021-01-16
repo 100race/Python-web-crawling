e9b4576bee93fd96cd347144c210f6ae909018cf
Kyoungju Baek <46726709+100race@users.noreply.github.com>
2021-01-16 23:49:58 +0900

    Create youtube-mp3-converter.py
    download youtube video and convert to mp3

---
+++ youtube-mp3-converter/youtube-mp3-converter.py
@@ -0,0 +1,21 @@
+import pytube
+
+
+url = 'https://www.youtube.com/watch?v=i7fnlJ60RO8&ab_channel=ionix95'
+yt = pytube.YouTube(url)
+videos = yt.streams.all()
+
+for i in range(len(videos)): #다운받을 수 있는 포맷들 출력
+    print(i,',',videos[i])
+
+down_dir = "C:\youtube"
+
+num = int(input("다운 받을 포맷/화질을 입력해주세요 :"))
+videos[num].download(down_dir)
+
+fName_new = input("저장할 mp3 파일 이름을 입력하세요 :")
+fName_ori = videos[num].default_filename
+
+subprocess.call(['ffmpeg', '-i', os.path.join(down_dir,fName_ori), os.path.join(down_dir,fName_new)])
+
+print("동영상 다운로드 및 mp3 변환 완료되었습니다!")
